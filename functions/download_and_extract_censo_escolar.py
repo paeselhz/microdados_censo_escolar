62c2a5297730692bcaba31f24c1768f5a2a44ea5
desktop <lhzpaese@gmail.com>
2020-11-07 16:38:37 -0300

    splitting repository functions and adding support until 2015

---
+++ functions/download_and_extract_censo_escolar.py
@@ -0,0 +1,70 @@
+import gc
+import os
+import glob
+import requests
+import zipfile
+
+os.environ["UNRAR_LIB_PATH"] = "/usr/lib/libunrar.so"
+
+from unrar import rarfile
+
+def download_and_extract_censo_escolar(ano_base):
+
+    os.makedirs('data/raw',
+                exist_ok = True)
+
+    url_download = "http://download.inep.gov.br/microdados/micro_censo_escolar_{ano}.zip".format(
+        ano = ano_base
+    )
+
+    url_check = requests.head(url_download)
+
+    if url_check.status_code is not 200:
+        print("Changing download url")
+        url_download = "http://download.inep.gov.br/microdados/microdados_educacao_basica_{ano}.zip".format(
+            ano = ano_base
+        )
+
+    print("Starting get request")
+
+    req = requests.get(url_download)
+
+    open("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base), 'wb').write(req.content)
+
+    print("Data exported to zip file")
+
+    with zipfile.ZipFile("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base), "r") as zip_file:
+        zip_file.extractall("data/raw/microdados_educacao_basica_{ano}/".format(ano = ano_base))
+
+    del req
+    gc.collect()
+
+    try:
+        os.remove("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base))
+    except:
+        print("File {filename} could not be removed".format(
+            filename = "microdados_educacao_basica_{ano}.zip".format(
+                 ano = ano_base
+            )
+        ))
+
+    print("Zip file extracted and removed")
+
+    list_of_files=glob.glob("data/raw/microdados_educacao_basica_{ano}/**".format(ano=ano_base),
+                            recursive=True)
+
+    print("List of rar files to extract:")
+
+    list_of_rar_files=[filename for filename in list_of_files if filename.endswith('.rar')]
+
+    print(list_of_rar_files)
+
+    for file_to_extract in list_of_rar_files:
+        with rarfile.RarFile(file_to_extract) as rar_file:
+            rar_file.extractall(os.path.dirname(file_to_extract))
+        try:
+            os.remove(file_to_extract)
+        except:
+            print("File {file} could not be removed").format(
+                file=file_to_extract.split(os.sep)[-1]
+            )
--- main.py
+++ main.py
@@ -1,83 +1,60 @@
 import os
 import glob
-import requests
-import zipfile
-import pandas as pd
 import json
+import gc
+import pyarrow as pa
+from dask import dataframe as dd
 
-os.environ["UNRAR_LIB_PATH"] = "/usr/lib/libunrar.so"
+ano_base = 2014
 
-from unrar import rarfile
+general_dtype_dict = json.load(open("support/files_dtype.json"))
 
-ano_base = 2007
+list_of_files=glob.glob("data/raw/microdados_educacao_basica_{ano}/**".format(ano=ano_base),
+                        recursive=True)
 
-with open("support/files_dtype.json") as json_data:
-    general_dtype_dict = json.load(json_data)
+list_of_csv_files=[filename for filename in list_of_files if filename.endswith((".csv", ".CSV"))]
 
-os.makedirs('data/raw',
-            exist_ok = True)
-
-url_download = "http://download.inep.gov.br/microdados/micro_censo_escolar_{ano}.zip".format(
-    ano = ano_base
-)
-
-url_check = requests.head(url_download)
-
-if url_check.status_code is not 200:
-    url_download = "http://download.inep.gov.br/microdados/microdados_educacao_basica_{ano}.zip".format(
-        ano = ano_base
-    )
-
-req = requests.get(url_download)
-
-open("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base), 'wb').write(req.content)
-
-with zipfile.ZipFile("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base), "r") as zip_file:
-    zip_file.extractall("data/raw/microdados_educacao_basica_{ano}/".format(ano = ano_base))
-
-try:
-    os.remove("data/raw/microdados_educacao_basica_{ano}.zip".format(ano = ano_base))
-except:
-    print("File {filename} could not be removed".format(
-        filename = "microdados_educacao_basica_{ano}.zip".format(
-             ano = ano_base
-        )
-    ))
-
-list_of_files = glob.glob("data/raw/microdados_educacao_basica_{ano}/**".format(ano = ano_base),
-                          recursive = True)
-
-list_of_rar_files = [filename for filename in list_of_files if filename.endswith('.rar')]
-
-for file_to_extract in list_of_rar_files:
-    with rarfile.RarFile(file_to_extract) as rar_file:
-        rar_file.extractall(os.path.dirname(file_to_extract))
-    try:
-        os.remove(file_to_extract)
-    except:
-        print("File {file} could not be removed").format(
-            file = file_to_extract.split(os.sep)[-1]
-        )
-
-list_of_files = glob.glob("data/raw/microdados_educacao_basica_{ano}/**".format(ano = ano_base),
-                          recursive = True)
-
-list_of_csv_files = [filename for filename in list_of_files if filename.endswith((".csv", ".CSV"))]
+print("List of csv files to convert to parquet")
 
 for file_to_import in list_of_csv_files:
-    dict_for_file_loading = general_dtype_dict.\
-        get(str(ano_base)).\
-        get(file_to_import.split(os.sep)[-1].replace(".csv", "").replace(".CSV", ""))
+    print("Converting to parquet")
+    print(file_to_import)
 
-    tbl = pd.read_csv(
+    file_filter=file_to_import.split(os.sep)[-1].replace(".csv", "").replace(".CSV", "").split("_")[0]
+
+    dict_for_file_loading=general_dtype_dict. \
+        get(str(ano_base)). \
+        get(file_filter)
+
+    pyarrow_schema={name: pa.from_numpy_dtype(value) for name, value in dict_for_file_loading.items()}
+
+    tbl=dd.read_csv(
         file_to_import,
-        sep = "|",
-        encoding = "latin_1",
-        dtype = dict_for_file_loading
+        sep="|",
+        encoding="latin_1",
+        dtype=dict_for_file_loading
     )
 
-    file_name_save = file_to_import.replace('.CSV', '.parquet').replace('.csv', '.parquet')
+    pyarrow_schema_filtered={"index": pa.from_numpy_dtype("Int64")}
 
-    tbl.reset_index().to_parquet(file_name_save, compression = "snappy")
+    pyarrow_schema_filtered.update({pa_key: pyarrow_schema.get(pa_key) for pa_key in tbl.columns})
+
+    pyarrow_tuples=pyarrow_schema_filtered.items()
+    pyarrow_list=list(pyarrow_tuples)
+
+    print("File Read")
+
+    file_name_save=file_to_import.replace('.CSV', '.parquet').replace('.csv', '.parquet')
+
+    tbl.reset_index().to_parquet(file_name_save, compression="snappy", engine="pyarrow", schema=pa.schema(pyarrow_list))
+
+    print("Cleaning mess")
+
+    del tbl
+    gc.collect()
 
     os.remove(file_to_import)
+
+# for year in range(2007, 2011):
+#     print(year)
+#     download_and_extract_censo_escolar(year)
